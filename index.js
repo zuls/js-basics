82809263f453868ca78d85c13e0ecb9dff25f545
zuls <zularbine@gmail.com>
2023-04-22 06:12:44 +1000

    array

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 // This is my first javascript code
-console.log("hello world")
+//console.log("hello world")
+
+//let name='kamal';
+//console.log(name)
+
+
+//let firstName = 'Zularbine';
+//let lastName='Kamal';
+
+//constant 
+
+const interastRate = 0.3;
+console.log(interastRate);
+
+//type of values or variable types 
+
+let name = 'Kamal';
+let age = 45;
+let isApproved = false; // Boolean Literal 
+let firstName = undefined;
+let lastName = null; 
+
+
+// object 
+let person = {
+    name: 'KAmal',
+    age: 45
+}
+
+console.log(person);
+
+
+// array
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 'green';
+selectedColors[3] = 1;
+console.log(selectedColors);
