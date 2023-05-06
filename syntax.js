17138716c16a9dab73a0a6d1b082c03590ce3cb9
zuls <zularbine@gmail.com>
2023-05-06 23:33:25 +0600

    data types

--- syntax.js
+++ syntax.js
@@ -12,3 +12,28 @@ if (window.localStorage){
   Multiline comments 
   2nd line of the multi line comment
  */
+
+  let message="Hello web", count=100;
+  let dollarDue = 500;
+  let dollarProfit = 40;
+  const dollarRate = 107;
+
+  console.log("Total dollar due to bdt ", dollarDue * dollarRate);
+
+  console.log(message+  "  " + count);
+
+  let counter = 120;
+  counter = true;
+  //counter = "hello";
+
+  console.log(counter);
+
+  let obj = null; 
+
+  console.log(typeof obj);
+
+  console.log('a'/2);
+
+  console.log("i\'m also a valid string");
+
+  
