50e84eee84ddb2a88e5516061ffc680d8c94609a
zuls <zularbine@gmail.com>
2023-05-06 21:17:49 +0600

    syntax

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function greet(name){
     console.log('from function ' + name);
 }
 
-greet('kamal')
+//greet('kamal')
 
-const numbers = new Array(1,2,3,4,5,6);
-console.log(numbers)
+//const numbers = new Array(1,2,3,4,5,6);
+//console.log(numbers)
---
+++ syntax.js
@@ -0,0 +1,14 @@
+let formatted = true;
+
+if (formatted) {
+    console.log("The code is easy to read");
+}
+
+if (window.localStorage){
+    console.log("Local storeage is supported");
+}
+
+/*
+  Multiline comments 
+  2nd line of the multi line comment
+ */
