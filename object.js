ceea3f8c651209238a7873791e16a79d6983d17a
zuls <zularbine@gmail.com>
2023-05-07 21:19:56 +0600

    objects

---
+++ object.js
@@ -0,0 +1,20 @@
+
+let person1={
+    firstName: 'John',
+    lastName: 'Doe',
+    address:{
+        building: '1',
+        road: '1/b',
+        block: 'b',
+        area: 'nobodoy',
+        thana: 'mohammadpur',
+        city: 'Dhaka',
+        zip: '1207'
+
+    }
+};
+
+console.log("object contains : " + person1.firstName);
+console.log("object contains : " + person1.address.area);
+
+console.log("from the object file");
