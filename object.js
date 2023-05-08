0a7d8d317f99d758c566822f83a0f525016f1ace
zuls <zularbine@gmail.com>
2023-05-08 21:25:41 +0600

    numbers

--- object.js
+++ object.js
@@ -16,5 +16,19 @@ let person1={
 
 console.log("object contains : " + person1.firstName);
 console.log("object contains : " + person1.address.area);
+console.log("object contains : " + person1['lastName']);
+console.log("object contains : " + person1['address'].road);
 
 console.log("from the object file");
+
+let nium = 0x1a; 
+console.log(nium);
+
+
+let amount = 0.2 + 0.1;
+
+console.log(amount)
+
+let pageView = 11920109n;
+
+console.log(pageView);
