594dbbf18b0998dbeea830c0fc62a57db889ce4b
zuls <zularbine@gmail.com>
2023-05-13 11:19:49 +0600

    number

--- boolean.js
+++ boolean.js
@@ -4,3 +4,14 @@ if (error) {
     console.log("there is error");
 }
 
+//string 
+
+let my_name = `Kamal`;
+let my_message = `Hi i am ${my_name}` + " " + "Hello " + `${my_name}`;
+
+console.log(my_message);
+
+
+let numberObject = new Number(100);
+
+console.log(numberObject.valueOf())
