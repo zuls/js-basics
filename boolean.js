f90516fc13d81e87e5d4d1d30b15574253dc6396
zuls <zularbine@gmail.com>
2023-05-14 10:42:35 +0600

    numbers

--- boolean.js
+++ boolean.js
@@ -15,3 +15,23 @@ console.log(my_message);
 let numberObject = new Number(100);
 
 console.log(numberObject.valueOf())
+
+let distance = 19.006;
+
+console.log(distance.toFixed(1));
+
+
+let z = 1000;
+
+console.log(z.toExponential());
+
+
+console.log(distance.toPrecision(5));
+
+
+let x_val = 10, y_val= 20;
+
+let z_val = x_val +y_val;
+
+console.log(z_val);
+
